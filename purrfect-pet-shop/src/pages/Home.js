043df0c68d9bd43c036750a959243488ba5079bc
Bre <breanna.schmidt12@gmail.com>
2022-09-01 21:47:31 -0400

    fixed nav bar, started home page

--- purrfect-pet-shop/src/pages/Home.js
+++ purrfect-pet-shop/src/pages/Home.js
@@ -1,3 +1,13 @@
 function Home() {
-	
+	return (
+		<section className="section">
+			
+			<h1 className="title">About Us</h1>
+			<h2 className="subtitle">
+			Lorem ipsum dolor sit amet. Sit modi mollitia et dolorem repellat sit autem alias ut rerum dolorem. At inventore possimus qui quos corrupti non voluptatibus minima et debitis placeat.
+			</h2>
+		</section>
+	)
 }
+
+export default Home
