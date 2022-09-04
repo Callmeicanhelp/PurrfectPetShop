073b3547e3e3b131121d0dc690aac34cdc458abf
Lonxyboi <drewthemachine22@gmail.com>
2022-09-04 10:46:57 -0400

    added features to allow for the react app to appear on the browser

--- server/server.js
+++ server/server.js
@@ -20,11 +20,11 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../purrfect-pet-shop/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../purrfect-pet-shop/build/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
