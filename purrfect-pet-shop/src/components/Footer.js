369526ecb86dbe190de9abd2144193f0d8ce581b
Lonxyboi <drewthemachine22@gmail.com>
2022-09-04 20:45:57 -0400

    added footer

---
+++ purrfect-pet-shop/src/components/Footer.js
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container text-center mb-5">
+        <h3>
+          Made with{' '}
+          <span
+            className="emoji"
+            role="img"
+            aria-label="heart"
+            aria-hidden="false"
+          >
+            ❤️
+          </span>{' '}
+          by Group 2
+        </h3>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
