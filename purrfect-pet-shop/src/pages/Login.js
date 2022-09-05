06b38ff899fd7895e4348c68af6fcff188156b97
Bre <breanna.schmidt12@gmail.com>
2022-09-05 14:26:17 -0400

    added script link for icons in index.js, login form renders and gives error message. Need to add error if login is incorrect, and if login is valid send them to different page

--- purrfect-pet-shop/src/pages/Login.js
+++ purrfect-pet-shop/src/pages/Login.js
@@ -1,10 +1,50 @@
-function login() {
+import {useState} from 'react'
+
+function Login() {
+	const [formState, setFormState] = useState({email:'', message:''});
+	const {email,password} = formState
+	const [error, setError] = useState('');
+
+	function validateEmail(email) {
+		var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(String(email).toLowerCase());
+	}
+
+	function change(e) {
+		if(e.target.name === 'email') {
+			const isValid = validateEmail(e.target.value);
+			console.log(isValid)
+			if(!isValid) {
+				setError('Invalid Email')
+			} else {
+				setError('')
+			}
+		} else {
+			if(!e.target.value.length) {
+				setError(`${e.target.name} is required`)
+			} else{
+				setError('')
+			}
+		}
+		setFormState({...formState, [e.target.name]: [e.target.value]});
+
+		if(!error) {
+			setFormState({...formState, [e.target.name]: [e.target.value]});
+		}
+	}
+
+	function submitChange(e) {
+		e.preventDefault();
+	}
 	return(
-		<div>
+		<form onSubmit={submitChange}>
+			<br></br>
+			<h1 className="title">Welcome Back! Please Login</h1>
+			<br></br>
 			<div className="field">
 				<p className="control has-icons-left has-icons-right">
-					<input className="input is-primary is-rounded is-medium" type="email" placeholder="Email"></input>
-					<span className="icon is-medium is-left">
+					<input className="input is-primary is-rounded is-medium" type="email" onChange={change} name="email" defaultValue={email} placeholder="Email"></input>
+					<span className="icon is-left">
 						<i className="fas fa-envelope"></i>
 					</span>
 					<span className="icon is-medium is-right">
@@ -14,14 +54,24 @@ function login() {
 			</div>
 			<div className="field">
 				<p className="control has-icons-left">
-					<input className="input is-primary is-rounded is-medium" type="password" placeholder="Password"></input>
+					<input className="input is-primary is-rounded is-medium" type="password" onChange={change} name="password" defaultValue={password} placeholder="Password"></input>
 					<span className="icon is-medium is-left">
 						<i className="fas fa-lock"></i>
 					</span>
 				</p>
 			</div>
-		</div>
+			<div className='errorMsg'>
+				{error && (
+					<div>
+						<p className='error-text is-danger'>{error}</p>
+					</div>
+				)}
+			</div>
+			<div className="control">
+				<button className="button is-link">Login</button>
+			</div>
+		</form>
 	)
 }
 
-export default login
+export default Login
