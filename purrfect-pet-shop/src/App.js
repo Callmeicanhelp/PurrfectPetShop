c68ce1fb91159f2f8e2a8c989e557b297642f59b
Lonxyboi <drewthemachine22@gmail.com>
2022-09-07 19:27:38 -0400

    help from tas for signup issues

--- purrfect-pet-shop/src/App.js
+++ purrfect-pet-shop/src/App.js
@@ -1,5 +1,14 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+  } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { StoreProvider } from './utils/GlobalState';
+
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Costumes from './pages/Costumes'
@@ -11,25 +20,51 @@ import Login from './pages/Login'
 // import Account from './pages/Account'
 import Footer from './components/Footer';
 
+const httpLink = createHttpLink({
+	uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 function App() {
   return (
-    <div className="App">
-      <header>
-					<Navbar />
-				<Router>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/costumes" element={<Costumes />} />
-						<Route path="/accessories" element={<Accessories />} />
-						<Route path="/toys" element={<Toys />} />
-						<Route path="/towers" element={<Towers />} />
-						<Route path="/signup" element={<Signup />} />
-						<Route path="/login" element={<Login />} />
-						{/* <Route pathh="/account" element={<Account />} */}
-					</Routes>
-				</Router>
-			</header>
-			<Footer/>
+    <div>
+		<ApolloProvider client={client}>
+			<div>
+				<StoreProvider>
+				<header>
+				<Navbar />
+					<Router>
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/costumes" element={<Costumes />} />
+							<Route path="/accessories" element={<Accessories />} />
+							<Route path="/toys" element={<Toys />} />
+							<Route path="/towers" element={<Towers />} />
+							<Route path="/signup" element={<Signup />} />
+							<Route path="/login" element={<Login />} />
+							{/* <Route pathh="/account" element={<Account />} */}
+						</Routes>
+					</Router>
+				</header>
+				<Footer/>
+				</StoreProvider>
+			</div>
+		</ApolloProvider>
     </div>
   );
 }
--- purrfect-pet-shop/src/pages/Signup.js
+++ purrfect-pet-shop/src/pages/Signup.js
@@ -1,89 +1,89 @@
 import React, { useState } from 'react';
-import Auth from '../utils/auth'
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
 
-const Signup = () => {
-  const [formState, setFormState] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
-  const [addUser, { error }] = useMutation(ADD_USER);
 
-  // update state based on form input changes
+function Signup(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName,
+      },
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
+
   const handleChange = (event) => {
     const { name, value } = event.target;
-
     setFormState({
       ...formState,
       [name]: value,
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const { data } = await addUser({
-        variables: { ...formState },
-      });
-
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   return (
-    <main className="flex-row justify-center mb-4">
-        <Link to="/login">← Go to Login</Link>
-        <div className="col-12 col-md-6">
-        <div className="card">
-          <h4 className="card-header">Sign Up</h4>
-          <div className="card-body">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="Your username"
-                name="username"
-                type="username"
-                id="username"
-                value={formState.username}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                id="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="password"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className="btn d-block w-100" type="submit">
-                Submit
-              </button>
-            </form>
+    <div className="container my-1">
+      <Link to="/login">← Go to Login</Link>
 
-            {error && <div>Signup failed</div>}
-          </div>
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            placeholder="First"
+            name="firstName"
+            type="firstName"
+            id="firstName"
+            onChange={handleChange}
+          />
         </div>
-      </div>
-    </main>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            placeholder="Last"
+            name="lastName"
+            type="lastName"
+            id="lastName"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
   );
-};
+}
 
 
 export default Signup;
