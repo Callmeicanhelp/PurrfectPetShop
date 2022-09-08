d5deac3b9968a76556b207d2aa86c38f7d0f730b
Lonxyboi <drewthemachine22@gmail.com>
2022-09-08 19:43:22 -0400

    messing with the login and signup

--- purrfect-pet-shop/src/pages/Login.js
+++ purrfect-pet-shop/src/pages/Login.js
@@ -1,95 +1,71 @@
-import {useState} from 'react'
-import Cookie from 'js-cookie'
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
 
+function Login(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN);
 
-function Login() {
-	const [loginInfo, setLoginInfo] = useState({email:"", password:""})
-	const {email,password} = loginInfo
-	const [error, setError] = useState('');
-	const [loginMessage, setLoginMessage] = useState({type:"", msg:""})
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-	function validateEmail(email) {
-		var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return re.test(String(email).toLowerCase());
-	}
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-	function change(e) {
-		if(e.target.name === 'email') {
-			const isValid = validateEmail(e.target.value);
-			console.log(isValid)
-			if(!isValid) {
-				setError('Invalid Email')
-			} else {
-				setError('')
-			}
-		} else {
-			if(!e.target.value.length) {
-				setError(`${e.target.name} is required`)
-			} else{
-				setError('')
-			}
-		}
-		setLoginInfo({...loginInfo, [e.target.name]: [e.target.value]});
+  return (
+    <div className="container my-1">
+      <Link to="/signup">← Go to Signup</Link>
 
-		if(!error) {
-			setLoginInfo({...loginInfo, [e.target.name]: [e.target.value]});
-		}
-	}
-
-	const submitLogin = async (e) => {
-		e.preventDefault();
-		setLoginMessage({type:"", msg:""})
-		const verify = await fetch("/api/user/auth", {
-			method:"POST",
-			headers: {"Content-Type": "application/json"},
-			body: JSON.stringify(loginInfo)
-		})
-		const verifyLogin = await verifyLogin.json()
-
-		if(verifyLogin === "success"){
-			Cookie.set("auth-token", verifyLogin.token)
-			setLoginMessage({type:"success", message:"Login successful! Welcome back! 😊"})
-		} else {
-			setLoginMessage({type:"danger", message:"Invalid login please try again"})
-		}
-		setLoginInfo({email:"", password:""})
-	}
-	return(
-		<form onSubmit={submitLogin}>
-			<br></br>
-			<h1 className="title">Welcome Back! Please Login</h1>
-			<br></br>
-			<div className="field">
-				<p className="control has-icons-left has-icons-right">
-					<input className="input is-primary is-rounded is-medium" type="email" onChange={change} name="email" value={loginInfo.email} placeholder="Email"></input>
-					<span className="icon is-left">
-						<i className="fas fa-envelope"></i>
-					</span>
-					<span className="icon is-medium is-right">
-						<i className="fas fa-check"></i>
-					</span>
-				</p>
-			</div>
-			<div className="field">
-				<p className="control has-icons-left">
-					<input className="input is-primary is-rounded is-medium" type="password" onChange={change} name="password" value={loginInfo.password} placeholder="Password"></input>
-					<span className="icon is-medium is-left">
-						<i className="fas fa-lock"></i>
-					</span>
-				</p>
-			</div>
-			<div className='errorMsg'>
-				{error && (
-					<div>
-						<p className='error-text is-danger'>{error}</p>
-					</div>
-				)}
-			</div>
-			<div className="control">
-				<button className="button is-link">Login</button>
-			</div>
-		</form>
-	)
+      <h2>Login</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email address:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
