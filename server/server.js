fe99ef55591bbe483b572fc4bbc34a1df90c9c7b
Callmeicanhelp <callmeicanhelp@gmail.com>
2022-09-09 00:53:22 -0400

    test

--- server/server.js
+++ server/server.js
@@ -13,15 +13,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../purrfect-pet-shop/build")));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../purrfect-pet-shop/build")));
+// }
 
-app.use(routes);
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../purrfect-pet-shop/build/index.html"));
-});
+// app.use(routes);
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../purrfect-pet-shop/build/index.html"));
+// });
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
