4be38985509586b0a4ca02606fb4689b1a9095a7
Callmeicanhelp <callmeicanhelp@gmail.com>
2022-09-09 01:27:06 -0400

    delete unused code

--- server/server.js
+++ server/server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const path = require("path");
 const routes = require("./routes");
 
-const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
