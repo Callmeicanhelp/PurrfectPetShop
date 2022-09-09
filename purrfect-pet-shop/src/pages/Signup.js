770d9bbe57c376a38f9eee9ca445a8e827c64c4f
Lonxyboi <drewthemachine22@gmail.com>
2022-09-08 20:19:40 -0400

    added a new signup page

--- purrfect-pet-shop/src/pages/Signup.js
+++ purrfect-pet-shop/src/pages/Signup.js
@@ -1,90 +1,37 @@
-import React, { useState } from 'react';
-import Auth from '../utils/auth'
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
-
-const Signup = () => {
-  const [formState, setFormState] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
-  const [addUser, { error }] = useMutation(ADD_USER);
-
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
-
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const { data } = await addUser({
-        variables: { ...formState },
-      });
-
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  return (
-    <main className="flex-row justify-center mb-4">
-        <Link to="/login">← Go to Login</Link>
-        <div className="col-12 col-md-6">
-        <div className="card">
-          <h4 className="card-header">Sign Up</h4>
-          <div className="card-body">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="Your username"
-                name="username"
-                type="username"
-                id="username"
-                value={formState.username}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                id="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="password"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className="btn d-block w-100" type="submit">
-                Submit
-              </button>
-            </form>
-
-            {error && <div>Signup failed</div>}
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-};
+import React, { Component } from "react";
 
 
-export default Signup;
+export default class SignUp extends Component {
+  render() {
+      return (
+          <form>
+              <h3>Register</h3>
 
+              <div className="form-group">
+                  <label>First name</label>
+                  <input type="text" className="form-control" placeholder="First name" />
+              </div>
+
+              <div className="form-group">
+                  <label>Last name</label>
+                  <input type="text" className="form-control" placeholder="Last name" />
+              </div>
+
+              <div className="form-group">
+                  <label>Email</label>
+                  <input type="email" className="form-control" placeholder="Enter email" />
+              </div>
+
+              <div className="form-group">
+                  <label>Password</label>
+                  <input type="password" className="form-control" placeholder="Enter password" />
+              </div>
+
+              <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+              <p className="forgot-password text-right">
+                  Already registered <a href="#">log in?</a>
+              </p>
+          </form>
+      );
+  }
+}
